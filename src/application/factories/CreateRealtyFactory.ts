e49f801492a121a7d160c888575c03f81db8a673
Nathan Araújo <74114687+natharaujos@users.noreply.github.com>
2024-06-26 10:26:26 -0300

    Update CreateRealtyFactory.ts

--- src/application/factories/CreateRealtyFactory.ts
+++ src/application/factories/CreateRealtyFactory.ts
@@ -3,7 +3,6 @@ import { ICreateRealty } from "../domain/useCases/ICreateRealty";
 import { CreateRealty } from "../usecases/CreateRealty";
 
 function createRealtyFactory(): ICreateRealty {
-  // Aqui você tem acesso à classe CreateRealtyService que implementa ICreateRealty
   const realtyDbAdapter = new RealtyDbAdapter();
 
   return new CreateRealty(realtyDbAdapter);
