cf24c840408c2a93eed174b9b60f503d74145801
Nathan Araújo <74114687+natharaujos@users.noreply.github.com>
2024-06-26 10:26:34 -0300

    Update GetRealtiesFactory.ts

--- src/application/factories/GetRealtiesFactory.ts
+++ src/application/factories/GetRealtiesFactory.ts
@@ -3,7 +3,6 @@ import { IGetRealties } from "../domain/useCases/IGetRealties";
 import { GetRealties } from "../usecases/GetRealties";
 
 export function getRealtiesFactory(): IGetRealties {
-  // Aqui você tem acesso à classe CreateRealtyService que implementa ICreateRealty
   const realtyDbAdapter = new RealtyDbAdapter();
 
   return new GetRealties(realtyDbAdapter);
